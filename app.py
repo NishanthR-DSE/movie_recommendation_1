ca761823ec77256757ee48088d750b37839d205f
NISHANTH R DATA SCIENCE ENGINEER <132241436+NishanthR-DSE@users.noreply.github.com>
2026-03-12 11:48:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# Load environment variables
+load_dotenv()
+
+# Get API key from .env
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+# Page Title
+st.title("Movie Recommender System using Gemini 🍿")
+
+# User Input
+user_input = st.text_input(
+    "Enter a movie title, genre or keywords (e.g. sci-fi movie)"
+)
+
+# Prompt Template
+template = PromptTemplate(
+    input_variables=["user_input"],
+    template="Suggest 5 movies similar to {user_input}"
+)
+
+# Gemini Model
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash-lite",
+    google_api_key=GOOGLE_API_KEY
+)
+
+# Generate recommendation
+if user_input:
+
+    chain = template | llm
+    response = chain.invoke({"user_input": user_input})
+
+    st.write("### Recommendations for you")
+    st.write(response.content)
+
+else:
+    st.write("")
